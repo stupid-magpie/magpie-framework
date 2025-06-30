b6c5fcf84be882e66afe11b07e7760fec239f711
syd <sydsleeper@gmail.com>
2025-06-30 04:49:04 -0400

    feat: auto copy dll task

--- dotnet/src/MagpieFramework/Native/MAGPIE_NATIVE.cs
+++ dotnet/src/MagpieFramework/Native/MAGPIE_NATIVE.cs
@@ -2,8 +2,7 @@ using System.Runtime.InteropServices;
 
 namespace MagpieFramework.Native;
 
-public static partial class MAGPIE_NATIVE
-{
+public static partial class MAGPIE_NATIVE {
     public const string LIB_NAME = "magpie_framework.dll";
 
     [LibraryImport(LIB_NAME, EntryPoint = "init_sdl")]
